7357fd8a85623220fefa8bc3ce8aa274a9d5d0ca
Riptir3 <thbence0116@gmail.com>
2026-03-25 16:57:24 +0100

    Refactoring GameResult model

--- GamePriceAggregatorApi/Models/GameResult.cs
+++ GamePriceAggregatorApi/Models/GameResult.cs
@@ -5,7 +5,6 @@
         public string Title { get; set; } = string.Empty;
         public string Price { get; set; } = "N/A";
         public string Store { get; set; } = string.Empty;
-        public string? AppId { get; set; }
         public string? ThumbnailUrl { get; set; }
     }
 }
